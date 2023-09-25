488378aef01e7baeab66859a9163cdd8b82d375c
SnehalMarutiSalokhe <snehal.maruti.salokhe@gmail.com>
2023-09-25 23:14:56 +0530

    added navbar

--- titans/src/App.js
+++ titans/src/App.js
@@ -1,9 +1,13 @@
-import './App.css';
+// import './App.css';
+// import 'bootstrap/dist/css/bootstrap.min.css';
 
+import  Navbar  from "./component/navbar/Navbar";
 function App() {
   return (
     <div className="App">
+       <Navbar/>
       <h1>Tech Titans</h1>
+     
     </div>
   );
 }
---
+++ titans/src/component/navbar/Navbar.jsx
@@ -0,0 +1,51 @@
+
+
+import React, { useState } from "react";
+import "./Navbar.css";
+import profile from "./profile.png";
+const NavbarComponent = () => {
+  const [navbarIsOpen, setNavbarIsOpen] = useState(false);
+  const [dropdownIsOpen, setDropdownIsOpen] = useState(false);
+
+  const handleToggleNavbar = () => {
+    setNavbarIsOpen(!navbarIsOpen);
+  };
+
+  const handleToggleDropdown = () => {
+    setDropdownIsOpen(!dropdownIsOpen);
+  };
+
+  return (
+    <div className="navbar">
+      <div className="nav-left">
+        <a href="/" className="nav-link">
+          Project
+        </a>
+        <a href="/" className="nav-link">
+          About Us
+        </a>
+        <a href="/" className="nav-link">
+          Contact
+        </a>
+      </div>
+      <div className="nav-search">
+        <input type="text" placeholder="Search" className="nav-link search-input" />
+        </div>
+        <div className="nav-link profile" onClick={handleToggleDropdown}>
+          <img src={profile} alt="Profile" className="profile-image" />
+          {dropdownIsOpen && (
+            <div className="dropdown">
+              <a href="/">Edit Profile </a>
+              <a href="/">MyProject</a>
+              <a href="/">Help</a>
+              <a href="/">Log Out</a>
+            </div>
+          )}
+        </div>
+      
+      
+    </div>
+  );
+};
+
+export default NavbarComponent;
